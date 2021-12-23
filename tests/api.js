202e7838899c3cea215e55ed0bebcd80b2102fab
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-12-23 13:26:51 +0100

    feat: add intial support for abort, retry and timeout

--- tests/api.js
+++ tests/api.js
@@ -2,8 +2,47 @@ const express = require("express");
 
 const api = express.Router();
 
+const answer = { answer: 42 };
+
 api.get("/42", (req, res) => {
-  res.json({ answer: 42 }).end();
+  res.json(answer);
+});
+
+api.get("/delay/:delay", (req, res) => {
+  setTimeout(() => {
+    res.json(answer);
+  }, parseInt(req.params.delay, 10));
+});
+
+const retry = {
+  ok: false,
+  timeout: false,
+};
+
+api.get("/retry/reset", (req, res) => {
+  retry.ok = false;
+  retry.timeout = false;
+  res.json(retry).end();
+});
+
+api.get("/retry", (req, res) => {
+  if (retry.ok) {
+    res.status(200).json(answer);
+  } else {
+    retry.ok = true;
+    res.status(502).json(retry);
+  }
+});
+
+api.get("/timeout-retry", (req, res) => {
+  if (retry.timeout) {
+    res.status(200).json(answer);
+  } else {
+    retry.timeout = true;
+    setTimeout(() => {
+      res.status(502).json(retry);
+    }, 3000);
+  }
 });
 
 module.exports = api;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,135 @@
+function request(url, options, controller) {
+  var timeout = (options && options.timeout) || 3000;
+  var timeoutAbort = false;
+  var timeoutInterval = setTimeout(function () {
+    timeoutAbort = true;
+    controller.abort();
+  }, timeout);
+
+  return fetch(url, { signal: controller.signal })
+    .catch(function (err) {
+      clearTimeout(timeoutInterval);
+      if (err.name === "AbortError") {
+        if (timeoutAbort) {
+          throw { name: "FartTimeoutError" };
+        }
+        throw { name: "FartAbortError" };
+      }
+      throw err;
+    })
+    .then(function (response) {
+      clearTimeout(timeoutInterval);
+      if (!response.ok) {
+        throw { name: "FartHttpStatusError" };
+      }
+      return response;
+    });
+}
+
+function fart(url, options) {
+  var controller = new AbortController();
+  var attempts = 0;
+  var maxAttempts = (options && options.maxAttempts) || 2;
+
+  var promise = request(url, options, controller).catch(function (err) {
+    if (err.name !== "FartAbortError" && attempts < maxAttempts) {
+      attempts += 1;
+      controller = new AbortController();
+      return request(url, options, controller);
+    }
+
+    throw err;
+  });
+
+  return {
+    then: function (fn) {
+      return promise.then(fn);
+    },
+    catch: function (fn) {
+      return promise.catch(fn);
+    },
+    abort: function () {
+      return controller.abort();
+    },
+  };
+}
+
 describe("fetch-art", () => {
+  const baseUrl = "http://localhost:8000/api";
+
   it("works", () => {
-    return fetch("http://localhost:8000/api/42")
+    return fart(`${baseUrl}/42`)
       .then((response) => response.json())
-      .then((data) => {
-        expect(data.answer).to.equal(42);
+      .then((data) => expect(data.answer).to.equal(42));
+  });
+
+  describe("Abort support", () => {
+    it("throws AbortError when abort is called", () => {
+      const promise = fart(`${baseUrl}/delay/200`);
+
+      promise.abort();
+
+      promise.catch((err) => {
+        expect(err.name).to.equal("FartAbortError");
       });
+    });
+
+    it("can be aborted after a timeout", (done) => {
+      const promise = fart(`${baseUrl}/delay/201`, { timeout: 80 });
+      setTimeout(() => {
+        promise.abort();
+        promise.catch((err) => {
+          expect(err.name).to.equal("FartAbortError");
+          done();
+        });
+      }, 100);
+    });
+  });
+
+  describe("Timeout support", () => {
+    it("throws TimeoutError when response takes too long", () => {
+      return fart(`${baseUrl}/delay/200`, { timeout: 100 }).catch((err) => {
+        expect(err.name).to.equal("FartTimeoutError");
+      });
+    });
+  });
+
+  describe("Retry support", () => {
+    beforeEach((done) => {
+      fart(`${baseUrl}/retry/reset`)
+        .then(() => done())
+        .catch(done);
+    });
+
+    it("automatic retry", () => {
+      return fart(`${baseUrl}/retry`)
+        .then((response) => response.json())
+        .then((data) => {
+          expect(data.answer).to.equal(42);
+        });
+    });
+
+    it("does not retry aborted requests", () => {
+      const promise = fart(`${baseUrl}/retry`);
+      promise.abort();
+
+      return promise.catch((err) => {
+        expect(err.name).to.equal("FartAbortError");
+      });
+    });
+
+    it("retries timedout requests", () => {
+      return fart(`${baseUrl}/timeout-retry`, { timeout: 50 })
+        .then((response) => response.json())
+        .then((data) => {
+          expect(data.answer).to.equal(42);
+        });
+    });
+
+    it("does not retry more than 2 times", () => {
+      return fart(`${baseUrl}/error`).catch((err) => {
+        expect(err.name).to.equal("FartHttpStatusError");
+      });
+    });
   });
 });
